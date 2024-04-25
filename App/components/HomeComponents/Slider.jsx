da59c0c367271c1dc409798704d3cd511d903852
akhilthej <67720804+akhilthej@users.noreply.github.com>
2024-04-26 02:40:04 +0530

    Slider Fix

--- App/components/HomeComponents/Slider.jsx
+++ App/components/HomeComponents/Slider.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, FlatList, Image } from 'react-native';
 
-const ImageSlider = ({ data }) => {
+
+const data = [
+  { id: '1', image: require('./blackhole.jpg') },
+  { id: '2', image: require('./blackhole.jpg') },
+  { id: '3', image: require('./blackhole.jpg') },
+  // Add more images as needed
+];
+
+const ImageSlider = () => {
   return (
-    <View  className='mt-4'>
+    <View className='mt-4'>
       <FlatList
         horizontal
         data={data}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.id} // Add keyExtractor
         renderItem={({ item }) => (
           <Image className='rounded-lg h-[200px] w-[330px] mr-3 object-cover' source={item.image} />
         )}
